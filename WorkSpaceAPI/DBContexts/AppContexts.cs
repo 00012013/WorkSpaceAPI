9c1b976a6ff56b7485339d6bcac7b9471f55d1af
00012013 <sayidalokhonkham@gmail.com>
2024-05-15 15:18:17 +0500

    created models ,connection string and DBContexts

---
+++ WorkSpaceAPI/DBContexts/AppContexts.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorkSpaceAPI.Model;
+
+namespace WorkSpaceAPI.DBContexts
+{
+    public class AppContexts : DbContext
+    {
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
+        public AppContexts(DbContextOptions<AppContexts> dbContext):base(dbContext) { 
+        }
+    }
+}
---
+++ WorkSpaceAPI/Model/Department.cs
@@ -0,0 +1,11 @@
+﻿namespace WorkSpaceAPI.Model
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public List<Employee> Employees { get; set; }
+    }
+}
